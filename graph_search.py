6659ebba6d1d2030310f090fdc0d070a2ba5f8f6
gautier <gautier.blandin@gmail.com>
2022-10-26 23:51:13 +0200

    Add bfs traversal function

--- graph_search.py
+++ graph_search.py
@@ -1,3 +1,7 @@
+import queue
+from typing import Callable
+
+
 def recursive_dfs(start: int, graph: list[list[int]]):
     """
     Traverse the graph with a DFS, and return all compete paths from the start node.
@@ -20,3 +24,25 @@ def recursive_dfs(start: int, graph: list[list[int]]):
 
     rec(start)
     return paths
+
+
+def bfs(start: int, graph: list[list[int]], callback: Callable[[int], None]):
+    """
+    Traverse the graph with a BFS, and call the callback function on each node.
+    Args:
+        start: The node to start the search from.
+        graph: The graph to search, represented as an adjacency list.
+        callback: The function to call on each node.
+    """
+    n = len(graph)
+    visited = [False] * n
+    q = queue.Queue()
+    q.put(start)
+
+    while q:
+        node = q.get()
+        callback(node)
+        for child in graph[node]:
+            if not visited[child]:
+                visited[child] = True
+                q.put(child)
