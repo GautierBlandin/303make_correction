d80f16550f74034ed20b25e59f10c33c752e8e9c
gautier <gautier.blandin@gmail.com>
2022-10-27 10:32:18 +0200

    Add tests for graph conversion utils

---
+++ test/test_conversion.py
@@ -0,0 +1,15 @@
+import graph_format_conversion
+
+
+def test_adjacency_matrix_to_adjacency_list():
+    graph = [
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0]
+    ]
+
+    adjacency_list = conversion.adjacency_matrix_to_adjacency_list(graph)
+    assert adjacency_list == [[2], [2, 5], [3], [], [5], [3]]
