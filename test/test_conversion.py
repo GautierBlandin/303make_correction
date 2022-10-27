0656c6eafcff1ab8b1567c09c63518e2ec9bdc64
gautier <gautier.blandin@gmail.com>
2022-10-27 10:41:51 +0200

    Fix import in graph conversion tests

--- test/test_conversion.py
+++ test/test_conversion.py
@@ -11,5 +11,5 @@ def test_adjacency_matrix_to_adjacency_list():
         [0, 0, 0, 1, 0, 0]
     ]
 
-    adjacency_list = conversion.adjacency_matrix_to_adjacency_list(graph)
+    adjacency_list = graph_format_conversion.adjacency_matrix_to_adjacency_list(graph)
     assert adjacency_list == [[2], [2, 5], [3], [], [5], [3]]
