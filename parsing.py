61b55147d81fb7a24dd9d25826047176af3d2b5b
gautier <gautier.blandin@gmail.com>
2022-10-27 09:44:50 +0200

    Finish the parse_file function

--- parsing.py
+++ parsing.py
@@ -1,3 +1,6 @@
+from dependency import Dependency
+
+
 def read_file(filename) -> str:
     try:
         with open(filename, 'r') as f:
@@ -7,7 +10,7 @@ def read_file(filename) -> str:
         exit(84)
 
 
-def parse_file(file_content: str):
+def parse_file(file_content: str) -> dict[str, Dependency]:
     """
     Parse the file content and return the dependency graph
     Args:
@@ -15,10 +18,18 @@ def parse_file(file_content: str):
     """
     lines = file_content.splitlines()
     lines = [line for line in lines if line != '']
+    dependencies = {}
 
     for i in range(len(lines) // 2):
         rule_line = lines[i * 2]
+        rule_name, deps = parse_rule(rule_line)
+        deps = sorted(deps)
         command_line = lines[i * 2 + 1]
+        dependencies[rule_name] = (Dependency(name=rule_name, dep_type="rule", dependencies=deps, command=command_line))
+        for dep in deps:
+            if dep not in dependencies:
+                dependencies[dep] = (Dependency(name=dep, dep_type="file"))
+    return dict(sorted(dependencies.items()))
 
 
 def parse_rule(rule_line: str) -> tuple[str, list[str]]:
