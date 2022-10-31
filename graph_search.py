03f2b9b47a0bf941409b022dd531172da6a09010
gautier <gautier.blandin@gmail.com>
2022-10-31 17:14:44 +0100

    Add BFS with topological order function

--- graph_search.py
+++ graph_search.py
@@ -45,4 +45,50 @@ def bfs_order(start: int, graph: list[list[int]]):
             if not visited[child]:
                 visited[child] = True
                 q.put(child)
-    return order
+    return order[1:]
+
+
+def bfs_with_top_order(start: int, graph: list[list[int]]) -> list[int]:
+    """
+    Execute a BFS traversal of the graph, while respecting the topological ordering of the graph.
+    Args:
+        start: The node to start the search from.
+        graph: The graph to search, represented as an adjacency list.
+    Returns:
+        A list of visited nodes, in topological order and BFS order.
+    """
+    # Find the in-degree of accessible nodes in the sub-graph.
+    n = len(graph)
+    in_degree = [0] * n
+
+    q = queue.Queue()
+    visited = [False] * n
+
+    q.put(start)
+    visited[start] = True
+
+    while not q.empty():
+        node = q.get()
+        for child in graph[node]:
+            if not visited[child]:
+                visited[child] = True
+                q.put(child)
+            in_degree[child] += 1
+
+    # Go through the graph again while respecting topological order.
+    order = []
+
+    q = queue.Queue()
+    q.put(start)
+
+    while not q.empty():
+        node = q.get()
+        order.append(node)
+        for child in graph[node]:
+            in_degree[child] -= 1
+            if in_degree[child] == 0:
+                q.put(child)
+
+    return order[1:]
+
+
